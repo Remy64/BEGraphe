026c48dbdf221dc60ea6464f7801f331d2eab7d5
Remy64 <35994712+Remy64@users.noreply.github.com>
2019-04-17 18:12:07 +0200

    début de Dicheutra

--- src/main/org/insa/algo/shortestpath/DijkstraAlgorithm.java
+++ src/main/org/insa/algo/shortestpath/DijkstraAlgorithm.java
@@ -1,5 +1,11 @@
 package org.insa.algo.shortestpath;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Stack;
+import org.insa.graph.*;
+import org.insa.algo.AbstractSolution.Status;
+import org.insa.algo.utils.BinaryHeap;
 public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
     public DijkstraAlgorithm(ShortestPathData data) {
@@ -11,6 +17,53 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
         ShortestPathData data = getInputData();
         ShortestPathSolution solution = null;
         // TODO:
+        Graph graph = data.getGraph();
+        BinaryHeap<Label> tas = new BinaryHeap<Label>();
+        
+        int nbNodes = graph.size();
+        
+        Label[] label = new Label[nbNodes];
+        for(int i = 0; i < nbNodes; i++) {
+        	label[i] = new Label(i);
+        }
+        int currentNode;
+        label[data.getOrigin().getId()].setCost(0);
+        tas.insert(label[data.getOrigin().getId()]);
+        boolean allMarked = false;
+        while(!allMarked) {
+        	currentNode = tas.deleteMin();
+        	label[currentNode].mark();
+        	for(Arc successeur : graph.get(currentNode).getSuccessors()) {
+        		this.notifyNodeReached(successeur.getDestination());
+        		int curSucc = successeur.getDestination().getId();
+        		if(!label[curSucc].isMarked()) {
+        			if(label[currentNode].getCost()+successeur.getMinimumTravelTime() < label[curSucc].getCost()) {
+        				label[curSucc].setCost(label[currentNode].getCost() + successeur.getMinimumTravelTime());
+        				System.out.println("Insertion dans le tas : Noeud numero"+successeur.getDestination().getId());
+        				tas.insert(successeur.getDestination().getId());
+        				label[curSucc].setFather(successeur);
+        			}
+        		}
+        	}
+        	allMarked = true;
+        	for(int j = 0; j < nbNodes && allMarked; j++) {
+        		allMarked &= label[j].isMarked();
+        	}
+        }
+        Label destination = label[data.getDestination().getId()];
+        int currNode = destination.getNode();
+        Stack<Arc> reversePath = new Stack<Arc>();
+        while(currNode != data.getOrigin().getId()) {
+        	reversePath.push(label[currNode].getFather());
+        }
+        ArrayList<Arc> liste_arcs = new ArrayList<Arc>();
+        while(!reversePath.isEmpty()) {
+        	liste_arcs.add(reversePath.pop());
+        }
+        Path path = new Path(graph, liste_arcs);
+   
+       solution = new ShortestPathSolution(data, Status.FEASIBLE, path);
+        
         return solution;
     }
 
--- src/main/org/insa/algo/shortestpath/Label.java
+++ src/main/org/insa/algo/shortestpath/Label.java
@@ -1,22 +1,44 @@
 package org.insa.algo.shortestpath;
 import org.insa.graph.*;
-public class Label {
-	private Node cur_sommet;
+public class Label implements Comparable<Label> {
+	private int cur_sommet;
 	private boolean marque;
-	private int cout;
+	private double cout;
 	private Arc pere;
 	
-	public Node getNode() {
+	public int getNode() {
 		return this.cur_sommet;
+	}
+	public Label(int node) {
+		this.cur_sommet = node;
+		this.cout = Double.POSITIVE_INFINITY;
+		this.marque = false;
+		this.pere = null; 
 	}
 	public Arc getFather() {
 		return this.pere;
 	}
+	public void setFather(Arc papa) {
+		this.pere = papa;
+	}
 	public boolean isMarked() {
 		return this.marque;
 	}
-	public int getCost() {
+	public void mark() {
+		this.marque = true;
+	}
+	public double getCost() {
 		return this.cout;
 	}	
-	
+	public void setCost(double cout) {
+		this.cout = cout;
+	}
+	public int compareTo(Label a) {
+		if(this.getCost() < a.getCost())
+			return -1;
+		else if(this.getCost() == a.getCost())
+			return 0;
+		else 
+			return 1;
+	}
 }
