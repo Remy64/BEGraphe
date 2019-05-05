fbd8b443fc39f257797a30ce8c3f8ccd25bb1a09
Remy64 <35994712+Remy64@users.noreply.github.com>
2019-05-05 23:47:29 +0200

    Add files via upload

--- src/main/org/insa/algo/shortestpath/DijkstraAlgorithm.java
+++ src/main/org/insa/algo/shortestpath/DijkstraAlgorithm.java
@@ -31,16 +31,17 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
         tas.insert(label[data.getOrigin().getId()]);
         boolean allMarked = false;
         while(!allMarked) {
-        	currentNode = tas.deleteMin();
+        	currentNode = tas.deleteMin().getNode();
         	label[currentNode].mark();
+        	System.out.println("Le noeud :"+currentNode+"vient d'être marqué");
         	for(Arc successeur : graph.get(currentNode).getSuccessors()) {
-        		this.notifyNodeReached(successeur.getDestination());
         		int curSucc = successeur.getDestination().getId();
         		if(!label[curSucc].isMarked()) {
         			if(label[currentNode].getCost()+successeur.getMinimumTravelTime() < label[curSucc].getCost()) {
+        				this.notifyNodeReached(successeur.getDestination());
         				label[curSucc].setCost(label[currentNode].getCost() + successeur.getMinimumTravelTime());
         				System.out.println("Insertion dans le tas : Noeud numero"+successeur.getDestination().getId());
-        				tas.insert(successeur.getDestination().getId());
+        				tas.insert(label[successeur.getDestination().getId()]);
         				label[curSucc].setFather(successeur);
         			}
         		}
@@ -50,11 +51,13 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
         		allMarked &= label[j].isMarked();
         	}
         }
+     
         Label destination = label[data.getDestination().getId()];
         int currNode = destination.getNode();
         Stack<Arc> reversePath = new Stack<Arc>();
         while(currNode != data.getOrigin().getId()) {
         	reversePath.push(label[currNode].getFather());
+        	currNode = label[currNode].getFather().getOrigin().getId();
         }
         ArrayList<Arc> liste_arcs = new ArrayList<Arc>();
         while(!reversePath.isEmpty()) {
@@ -68,3 +71,5 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
     }
 
 }
+    
+
