fcc7a2d7e6e3817147cf215a8708bdb73b9ddfaf
Remy64 <35994712+Remy64@users.noreply.github.com>
2019-05-06 10:49:11 +0200

    Add files via upload

---
+++ src/test/org/insa/algo/djikstraTest.java
@@ -0,0 +1,10 @@
+package org.insa.algo;
+
+public class djikstraTest {
+
+	String mapName1, mapName2, mapName3;
+	mapName1="/home/commetud/3eme Annee MIC/Graphes-et-Algorithmes/Maps/toulouse.mapfg";
+	mapName2="/home/commetud/3eme Annee MIC/Graphes-et-Algorithmes/Maps/";
+	mapName3="/home/commetud/3eme Annee MIC/Graphes-et-Algorithmes/Maps/";
+	
+}
