55eb55a2eebca7dbd9067f8b0dab2c2b04f88412
jb332 <36227037+jb332@users.noreply.github.com>
2019-05-09 13:41:59 +0200

    Update DijkstraAlgorithm.java

--- src/main/org/insa/algo/shortestpath/DijkstraAlgorithm.java
+++ src/main/org/insa/algo/shortestpath/DijkstraAlgorithm.java
@@ -6,6 +6,7 @@ import java.util.Stack;
 import org.insa.graph.*;
 import org.insa.algo.AbstractSolution.Status;
 import org.insa.algo.utils.BinaryHeap;
+import org.insa.algo.utils.EmptyPriorityQueueException;
 public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
     public DijkstraAlgorithm(ShortestPathData data) {
@@ -30,46 +31,58 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
         label[data.getOrigin().getId()].setCost(0);
         tas.insert(label[data.getOrigin().getId()]);
         boolean allMarked = false;
-        while(!allMarked) {
-        	currentNode = tas.deleteMin().getNode();
-        	label[currentNode].mark();
-        	System.out.println("Le noeud :"+currentNode+"vient d'être marqué");
-        	for(Arc successeur : graph.get(currentNode).getSuccessors()) {
-        		int curSucc = successeur.getDestination().getId();
-        		if(!label[curSucc].isMarked()) {
-        			if(label[currentNode].getCost()+successeur.getMinimumTravelTime() < label[curSucc].getCost()) {
-        				this.notifyNodeReached(successeur.getDestination());
-        				label[curSucc].setCost(label[currentNode].getCost() + successeur.getMinimumTravelTime());
-        				System.out.println("Insertion dans le tas : Noeud numero"+successeur.getDestination().getId());
-        				tas.insert(label[successeur.getDestination().getId()]);
-        				label[curSucc].setFather(successeur);
-        			}
-        		}
-        	}
-        	allMarked = true;
-        	for(int j = 0; j < nbNodes && allMarked; j++) {
-        		allMarked &= label[j].isMarked();
-        	}
+        while(!label[data.getDestination().getId()].isMarked()) {
+        	try {
+	        	currentNode = tas.deleteMin().getNode();
+	        	
+	        	
+	        	label[currentNode].mark();
+	        	System.out.println("Le noeud :"+currentNode+"vient d'etre marque. Son cout est "+label[currentNode].getCost());
+	        	for(Arc successeur : graph.get(currentNode).getSuccessors()) {
+	        		int curSucc = successeur.getDestination().getId();
+	        		if(!label[curSucc].isMarked() && data.isAllowed(successeur)) {
+	        			if(label[currentNode].getCost()+data.getCost(successeur) < label[curSucc].getCost()) {
+	        				this.notifyNodeReached(successeur.getDestination());
+	        				label[curSucc].setCost(label[currentNode].getCost() + data.getCost(successeur));
+	        				//System.out.println("Insertion dans le tas : Noeud numero"+successeur.getDestination().getId());
+	        				tas.insert(label[successeur.getDestination().getId()]);
+	        				label[curSucc].setFather(successeur);
+	        			}
+	        			System.out.println("Le tas a une taille de "+tas.size());
+	        		}
+	        	}
+	        	allMarked = true;
+	        	for(int j = 0; j < nbNodes && allMarked; j++) {
+	        		allMarked &= label[j].isMarked();
+	        	}
+	        }
+	     
+	        catch(EmptyPriorityQueueException e) {
+	    		ArrayList<Node> noeuds = new ArrayList<Node>();
+	    		Path path = Path.createFastestPathFromNodes(graph, noeuds);
+	    		solution = new ShortestPathSolution(data, Status.INFEASIBLE, path);
+	    		return solution;
+	    	}
         }
      
         Label destination = label[data.getDestination().getId()];
         int currNode = destination.getNode();
-        Stack<Arc> reversePath = new Stack<Arc>();
+        Stack<Node> reversePath = new Stack<Node>();
         while(currNode != data.getOrigin().getId()) {
-        	reversePath.push(label[currNode].getFather());
+        	reversePath.push(label[currNode].getFather().getOrigin());
         	currNode = label[currNode].getFather().getOrigin().getId();
         }
-        ArrayList<Arc> liste_arcs = new ArrayList<Arc>();
+        ArrayList<Node> liste_noeuds = new ArrayList<Node>();
         while(!reversePath.isEmpty()) {
-        	liste_arcs.add(reversePath.pop());
+        	liste_noeuds.add(reversePath.pop());
         }
-        Path path = new Path(graph, liste_arcs);
+        Path path = Path.createShortestPathFromNodes(graph, liste_noeuds);
    
        solution = new ShortestPathSolution(data, Status.FEASIBLE, path);
+       System.out.println("Ce chemin contient : "+liste_noeuds.size()+" arcs");
+       
         
         return solution;
     }
 
 }
-    
-
