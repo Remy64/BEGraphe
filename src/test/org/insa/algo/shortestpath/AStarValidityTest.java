8f8c1c3cedb87a46ffe9510326d343f275af0b1e
jb332 <36227037+jb332@users.noreply.github.com>
2019-05-26 19:05:14 +0200

    Add files via upload

---
+++ src/test/org/insa/algo/shortestpath/AStarValidityTest.java
@@ -0,0 +1,18 @@
+package org.insa.algo.shortestpath;
+
+import org.insa.graph.*;
+
+public class AStarValidityTest extends DijkstraValidityTest {
+	
+	public static Path[] getShortestPaths(String mapName, int originId, int destId, int mode) {
+		try {
+			Path[] paths = new Path[2];
+			paths[0] = getShortestPath(mapName, originId, destId, mode, 'b');
+			paths[1] = getShortestPath(mapName, originId, destId, mode, 'a');
+			return paths;
+		} catch(Exception e) {
+			System.out.println(e.getMessage());
+			return null;
+		}
+	}
+}
